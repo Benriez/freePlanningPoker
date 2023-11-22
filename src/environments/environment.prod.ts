a26ec79c1499ac2d4ff4e5cfff6727d3f2e655ce
Benriez <stberiez@stud.h-da.de>
2023-11-22 15:57:50 +0100

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  WS_URL:'wss://freeplanning    /ws/',
-  BASE_URL:'http://localhost:4200/'
+  WS_URL:'wss://ffp-django.cloud.code-server.de//ws/',
+  BASE_URL:'https://ffp-django.cloud.code-server.de/admin/'
 };
