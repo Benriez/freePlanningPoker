94954532f56e3244870abfad5f74f1bb27b4e141
Benriez <stberiez@stud.h-da.de>
2023-11-22 16:08:12 +0100

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  WS_URL:'wss://ffp-django.cloud.code-server.de//ws/',
-  BASE_URL:'https://ffp-django.cloud.code-server.de/admin/'
+  WS_URL:'wss://ffp-django.cloud.code-server.de/ws/',
+  BASE_URL:'https://ffp-django.cloud.code-server.de/'
 };
