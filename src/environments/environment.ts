49ddf77c694002d81ebd4b2ab1bfa67d1a0af6df
Benriez <stberiez@stud.h-da.de>
2023-11-22 18:58:00 +0100

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  // WS_URL:'ws://127.0.0.1:8000/ws/',
-  // BASE_URL:'http://localhost:4200/'
-  WS_URL:'wss://ffp-django.cloud.code-server.de/ws/',
-  BASE_URL:'https://ffp-django.cloud.code-server.de/',
+  WS_URL:'ws://127.0.0.1:8000/ws/',
+  BASE_URL:'http://localhost:4200/',
+  // WS_URL:'wss://ffp-django.cloud.code-server.de/ws/',
+  // BASE_URL:'https://ffp-django.cloud.code-server.de/',
   BASE_APP_URL:'https://ffp-app.cloud.code-server.de/'
 };
 
