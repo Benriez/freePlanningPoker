1a7a67c55a975acd4029f0adf8219b0c529d04dc
Benriez <stberiez@stud.h-da.de>
2023-11-23 16:00:45 +0100

    updte

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
-  WS_URL:'wss://fpp-django.cloud.code-server.de/ws/',
-  BASE_URL:'https://fpp-django.cloud.code-server.de/',
-  BASE_APP_URL:'https://fpp-django.cloud.code-server.de/'
-
+  WS_URL:'wss://free-planning-poker.cloud.code-server.de/ws/',
+  BASE_URL:'https://free-planning-poker.cloud.code-server.de/',
+  BASE_APP_URL:'https://free-planning-poker.cloud.code-server.de/'
 
 };
