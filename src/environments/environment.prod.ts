5c076df7977434c0b5dc45a5863cfd0012b987a7
Benriez <stberiez@stud.h-da.de>
2023-11-28 23:16:10 +0100

    update url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  WS_URL:'wss://free-planning-poker.cloud.code-server.de/ws/',
-  BASE_URL:'https://free-planning-poker.cloud.code-server.de/',
-  BASE_APP_URL:'https://free-planning-poker.cloud.code-server.de/'
+  WS_URL:'wss://freeplanningpoker.de/ws/',
+  BASE_URL:'https://freeplanningpoker.de/',
+  BASE_APP_URL:'https://freeplanningpoker.de/'
 
 };
